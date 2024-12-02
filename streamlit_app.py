f9624ec9ade09cb55d249176ecc7c27bfb0fbcd9
N Hameleers <niels.hameleers@gmail.com>
2024-12-02 22:11:52 +0000

    add sunburst chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,30 @@
 import streamlit as st
+import plotly.graph_objects as go
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Pluswijken Wijkscan")
+
+wijken = ['Kerkrade Rolduckerveld',
+          'Eijsden']
+
+wijk = st.selectbox('Selecteer uw wijk', options=wijken)
+
+# Data for the chart
+valuedict = {'Kerkrade Rolduckerveld': [10, 4, 6, 8, 3, 3, 2],
+             'Eijsden': [7, 2, 5, 9, 4, 3, 2]}
+labels = ["Eigen kracht en persoonlijke sociale basis", "Child A1", "Child A2", "Gemeenschappelijke sociale basis", "Child B1", "Child B2", "Child B3"]
+parents = ["", "Eigen kracht en persoonlijke sociale basis", "Eigen kracht en persoonlijke sociale basis", "", "Gemeenschappelijke sociale basis", "Gemeenschappelijke sociale basis", "Gemeenschappelijke sociale basis"]
+values = valuedict[wijk]
+
+# Create the Sunburst chart
+fig = go.Figure(go.Sunburst(
+    labels=labels,
+    parents=parents,
+    values=values,
+    branchvalues="total",  # Use "total" or "remainder" for value computation
+    sort=False  # Ensure consistent positioning
+))
+
+fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
+
+# Display the chart in Streamlit
+st.plotly_chart(fig)

eaacb289b57354e3858786fa525052b7ed797807
N Hameleers <niels.hameleers@gmail.com>
2024-12-02 22:13:29 +0000

    add 3rd category to sunburst

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,10 @@ wijken = ['Kerkrade Rolduckerveld',
 wijk = st.selectbox('Selecteer uw wijk', options=wijken)
 
 # Data for the chart
-valuedict = {'Kerkrade Rolduckerveld': [10, 4, 6, 8, 3, 3, 2],
-             'Eijsden': [7, 2, 5, 9, 4, 3, 2]}
-labels = ["Eigen kracht en persoonlijke sociale basis", "Child A1", "Child A2", "Gemeenschappelijke sociale basis", "Child B1", "Child B2", "Child B3"]
-parents = ["", "Eigen kracht en persoonlijke sociale basis", "Eigen kracht en persoonlijke sociale basis", "", "Gemeenschappelijke sociale basis", "Gemeenschappelijke sociale basis", "Gemeenschappelijke sociale basis"]
+valuedict = {'Kerkrade Rolduckerveld': [10, 4, 6, 8, 3, 3, 2, 2, 2],
+             'Eijsden': [7, 2, 5, 9, 4, 3, 2, 3, 3]}
+labels = ["Eigen kracht en persoonlijke sociale basis", "Child A1", "Child A2", "Gemeenschappelijke sociale basis", "Child B1", "Child B2", "Child B3", "Zorg en welzijn", "Child C1"]
+parents = ["", "Eigen kracht en persoonlijke sociale basis", "Eigen kracht en persoonlijke sociale basis", "", "Gemeenschappelijke sociale basis", "Gemeenschappelijke sociale basis", "Gemeenschappelijke sociale basis", "", "Zorg en welzijn"]
 values = valuedict[wijk]
 
 # Create the Sunburst chart

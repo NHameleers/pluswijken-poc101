c6f4e614895d1f69c875362f12cdf5cc1cf758f3
N Hameleers <niels.hameleers@gmail.com>
2024-12-09 09:04:24 +0000

    add treemap plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
 
 st.title("Pluswijken Wijkscan")
 'LET OP: Dit is een prototype in ontwikkeling dat gebruikt maakt van fictieve data'
@@ -11,10 +13,26 @@ wijken = ['Kerkrade Rolduckerveld',
 wijk = st.selectbox('Selecteer uw wijk', options=wijken)
 
 # Data for the chart
-valuedict = {'Kerkrade Rolduckerveld': [10, 4, 6, 8, 3, 3, 2, 2, 2],
+valuedict = {'Kerkrade Rolduckerveld': [15, 9, 6, 15, 5, 5, 5, 15, 15],
              'Eijsden': [7, 2, 5, 9, 4, 3, 2, 3, 3]}
-labels = ["Eigen kracht en persoonlijke sociale basis", "Child A1", "Child A2", "Gemeenschappelijke sociale basis", "Child B1", "Child B2", "Child B3", "Zorg en welzijn", "Child C1"]
-parents = ["", "Eigen kracht en persoonlijke sociale basis", "Eigen kracht en persoonlijke sociale basis", "", "Gemeenschappelijke sociale basis", "Gemeenschappelijke sociale basis", "Gemeenschappelijke sociale basis", "", "Zorg en welzijn"]
+labels = ["Eigen kracht en persoonlijke sociale basis",
+                "Gemiddeld aantal uur mantelzorg geven per week",
+                "Percentage zeer eenzaam",
+            "Gemeenschappelijke sociale basis",
+                "Child B1",
+                "Child B2",
+                "Child B3",
+            "Zorg en welzijn",
+                "Child C1"]
+parents = ["",
+                "Eigen kracht en persoonlijke sociale basis",
+                "Eigen kracht en persoonlijke sociale basis",
+            "",
+                "Gemeenschappelijke sociale basis",
+                "Gemeenschappelijke sociale basis",
+                "Gemeenschappelijke sociale basis",
+            "",
+                "Zorg en welzijn"]
 values = valuedict[wijk]
 
 # Create the Sunburst chart
@@ -23,10 +41,41 @@ fig = go.Figure(go.Sunburst(
     parents=parents,
     values=values,
     branchvalues="total",  # Use "total" or "remainder" for value computation
-    sort=False  # Ensure consistent positioning
+    sort=False,  # Ensure consistent positioning
+    # marker=dict(
+    #     colors=values,  # Use values to determine the color intensity
+    #     colorscale="RdYlGn"  # Use a predefined color scale
+    # )
 ))
 
 fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
 
 # Display the chart in Streamlit
 st.plotly_chart(fig)
+
+
+'## Alternatieve weergave'
+
+treemap_df = pd.DataFrame({
+    "Category": parents,  # Parent category
+    "Subcategory": labels,  # Child category
+    "Values": values  # Values
+})
+treemap_df = treemap_df.loc[treemap_df['Category'] != "", ]
+
+# Plot the Treemap
+fig = px.treemap(
+    treemap_df,
+    path=["Category", "Subcategory"],  # Define hierarchy
+    values="Values",  # Sizes of rectangles
+    color="Values",  # Use values to set color intensity
+    # color_continuous_scale="RdYlGn"
+)
+
+# Update layout
+fig.update_layout(margin=dict(t=30, l=0, r=0, b=0))
+
+# Display the chart in Streamlit
+st.plotly_chart(fig)
+
+
